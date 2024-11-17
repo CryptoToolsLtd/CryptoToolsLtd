e0e51bc612fa2b4e00194e0c4900d8abab552b7b
Lê Thành Đạt <22028189@vnu.edu.vn>
2024-11-17 22:18:12 +0700

    finish clearly three cryptography and three signatures

--- website/static/js/ecc.js
+++ website/static/js/ecc.js
@@ -1,12 +1,13 @@
 function showOption(OptionNumber) {
-    // Hide both pages
+    
     document.getElementById("option1").classList.add("hidden");
     document.getElementById("option2").classList.add("hidden");
 
-    // Show the selected page
     if (OptionNumber === 1) {
         document.getElementById("option1").classList.remove("hidden");
     } else if (OptionNumber === 2) {
         document.getElementById("option2").classList.remove("hidden");
     }
+    
 }
+
